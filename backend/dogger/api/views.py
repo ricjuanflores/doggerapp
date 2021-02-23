d1d8111c1358d0428b79c55d82e049a13d3c63c2
Juan R. Flores <juanflores.ric@gmail.com>
2021-02-23 16:16:46 -0500

    clean

--- backend/dogger/api/views.py
+++ backend/dogger/api/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from .permissions import IsOwnerUser, IsWalkerUser
-# Create your views here.
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
 
 from .serializers import UserSerializer, UserSerializerWithToken
 from .serializers import DogSerializer, ServiceSerializer
 
-from django.contrib.auth.hashers import make_password
 from .models import User, Dog, Service, Schedule
-from rest_framework import status
+from .permissions import IsOwnerUser, IsWalkerUser
 from .utils import validate_limite_dogs
 
 
